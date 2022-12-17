15d619c962b43fff273355e44f315e90da8abfeb
GrzegorzJendernal <g.jendernal@gmail.com>
2022-12-17 20:11:34 +0100

    added errorPage component

---
+++ src/common/ErrorPage/index.js
@@ -0,0 +1,16 @@
+import { BiggerText, Button, ButtonText, Sign, Text, Wrapper } from "./styled";
+
+const ErrorPage = () => (
+    <Wrapper>
+        <Sign />
+        <BiggerText>Ooops! Something went wrong... </BiggerText>
+        <Text>Please check your network connection<br /> and try again</Text>
+        <Button>
+            <ButtonText>
+                Back to home page
+            </ButtonText>
+        </Button>
+    </Wrapper >
+);
+
+export default ErrorPage;
