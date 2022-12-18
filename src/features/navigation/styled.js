96e9e0f2ec31ae743da7ee279cfea69c5ec9d146
Filip Buchwald <fixu1998@gmail.com>
2022-12-18 22:19:49 +0100

    created navigation

---
+++ src/features/navigation/styled.js
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    padding: 0px;
+    gap: 2px;
+  }
+`;
+
+export const Item = styled.li`
+  list-style: none;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.color.common.white};
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 600;
+  padding: 8px 24px;
+
+  &.active {
+    border: 1px solid #ffffff;
+    border-radius: 24px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 12px;
+    line-height: 18px;
+    padding: 8px 16px;
+  }
+`;
