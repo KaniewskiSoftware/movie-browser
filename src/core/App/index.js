0eb5eab94b8e9158a4a0ad22dfc63f6672dc3cf3
GrzegorzJendernal <g.jendernal@gmail.com>
2022-12-27 20:08:26 +0100

    added hashrouter to app

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -4,6 +4,8 @@ import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import MovieListPage from "../../features/movieList/MovieListPage";
 import Header from "../../common/Header";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import ErrorPage from "../../common/states/ErrorPage";
 
 function App() {
   return (
@@ -11,7 +13,19 @@ function App() {
       <Normalize />
       <GlobalStyle />
       <Header />
-      <MovieListPage />
+      <HashRouter>
+        <Switch>
+          <Route path="/movies">
+            <MovieListPage />
+          </Route>
+          <Route path="/people">
+            <ErrorPage />
+          </Route>
+          <Route path="/">
+            <Redirect to="/movies" />
+          </Route>
+        </Switch>
+      </HashRouter>
     </ThemeProvider>
   );
 }
