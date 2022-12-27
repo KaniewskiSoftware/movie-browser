8674214e8b04f5f1833820822277754f825ccae7
GrzegorzJendernal <g.jendernal@gmail.com>
2022-12-27 20:58:36 +0100

    added functions to input

--- src/common/Header/Search/index.js
+++ src/common/Header/Search/index.js
@@ -1,12 +1,26 @@
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
 import svg from "./Search.svg"
+import searchQueryParamName from "./searchQueryParamName";
 import { Input, InputWrapper, Loupe } from "./styled";
 
 const Search = () => {
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
+    };
 
     return (
         <InputWrapper>
             <Loupe src={svg} />
-            <Input placeholder="Search for movies..." />
+            <Input
+                placeholder="Search for movies..."
+                value={query || ""}
+                onChange={onInputChange} />
         </InputWrapper>
     );
 };
