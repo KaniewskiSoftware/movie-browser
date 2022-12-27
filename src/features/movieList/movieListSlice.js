f3d1c2592e84caa81359373a6e8d75af5d5c3ebf
GrzegorzJendernal <g.jendernal@gmail.com>
2022-12-27 20:08:42 +0100

    added new selector - selectMoviesByQuery

--- src/features/movieList/movieListSlice.js
+++ src/features/movieList/movieListSlice.js
@@ -18,7 +18,7 @@ const movieListSlice = createSlice({
     fetchMoviesError: (state) => {
       state.status = "error";
     },
-    fetchGenres: () => {},
+    fetchGenres: () => { },
     fetchGenresSuccess: (state, { payload: genres }) => {
       state.genres = genres;
     },
@@ -43,5 +43,13 @@ export const selectStatus = (state) => selectMovieListState(state).status;
 export const selectMovies = (state) => selectMovieListState(state).movies;
 export const selectGenres = (state) => selectMovieListState(state).genres;
 export const selectIsGenres = (state) => selectMovieListState(state).isGenres;
+
+export const selectMoviesByQuery = (state, query) => {
+  const movies = selectMovies(state);
+  if (!query || query.trim() === "") {
+    return movies
+  }
+  return selectMovies(state).filter(({ original_title }) => original_title.toUpperCase().includes(query.trim().toUpperCase()))
+};
 
 export default movieListSlice.reducer;
