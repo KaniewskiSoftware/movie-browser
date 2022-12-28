9a397b61d766903f4a32ab17c776b27341f99feb
GrzegorzJendernal <g.jendernal@gmail.com>
2022-12-28 11:27:51 +0100

    styled Loader

--- src/common/states/Loader/index.js
+++ src/common/states/Loader/index.js
@@ -1,14 +1,14 @@
-import { useQueryParameter } from "../../Header/Search/queryParameters";
+import { useQueryParameter, searchQueryParamName } from "../../Header/Search/queryParameters";
 import Header from "../Header";
 import { Spinner, SpinnerBox, Wrapper } from "./styled";
 
 const Loader = () => {
-    const query = useQueryParameter();
+    const searchQuery = useQueryParameter(searchQueryParamName);
 
     return (
         <Wrapper>
-            <Header title={!query ? "" : `Search results for ${query}`} />
-            <SpinnerBox>
+            <Header title={!searchQuery ? "" : `Search results for "${searchQuery}"`} />
+            <SpinnerBox hasTitle={!!searchQuery}>
                 <Spinner />
             </SpinnerBox>
         </Wrapper>
--- src/common/states/Loader/styled.js
+++ src/common/states/Loader/styled.js
@@ -10,7 +10,7 @@ const SpinnerBox = styled.div`
   display: grid;
   justify-content: center;
   align-items: center;
-  margin-top: 120px;
+  margin-top: ${({ hasTitle }) => hasTitle ? '120px' : '219px'};
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     margin-top: 24px;
