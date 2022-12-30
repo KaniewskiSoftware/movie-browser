42084e9b9cb4301d62d84486d5e2303716ca8b7c
Filip Buchwald <fixu1998@gmail.com>
2022-12-30 17:17:02 +0100

    adjusted router to the new version

--- src/features/navigation/Navigation/index.js
+++ src/features/navigation/Navigation/index.js
@@ -1,19 +1,16 @@
-import { HashRouter } from "react-router-dom";
 import { Item, List, Nav, StyledNavLink } from "./styled";
 
 const Navigation = () => (
-  <HashRouter>
-    <Nav>
-      <List>
-        <Item>
-          <StyledNavLink to={"/movies"}>Movies</StyledNavLink>
-        </Item>
-        <Item>
-          <StyledNavLink to={"/people"}>People</StyledNavLink>
-        </Item>
-      </List>
-    </Nav>
-  </HashRouter>
+  <Nav>
+    <List>
+      <Item>
+        <StyledNavLink to={"/movies"}>Movies</StyledNavLink>
+      </Item>
+      <Item>
+        <StyledNavLink to={"/people"}>People</StyledNavLink>
+      </Item>
+    </List>
+  </Nav>
 );
 
 export default Navigation;
