8c4da1b3f02f39253312140a75dcff02d87a2ddb
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-03 21:22:30 +0100

    added people conponent

--- src/features/peopleList/PeoplePage/People/index.js
+++ src/features/peopleList/PeoplePage/People/index.js
@@ -1,6 +1,36 @@
+import PageTitle from "../../../../common/PageTitle";
+import { Wrapper } from "../../../../common/Wrapper";
+import { CreditContainer, CreditTiles } from "../../../../common/Credits/CreditEssentials";
+import Credits from "../../../../common/Credits";
+import { useSelector } from "react-redux";
+import { selectPeople, selectTotalPages } from "../../peopleSlice";
+import Footer from "../../../../common/Footer";
+
 const People = () => {
+    const people = useSelector(selectPeople);
+    const totalPages = useSelector(selectTotalPages);
+
     return (
-        <>nanana</>
+        <>
+            <Wrapper>
+                <CreditContainer>
+                    <PageTitle title={"Popular people"} />
+                    <CreditTiles>
+                        {people.map((person) => (
+                            <Credits
+                                key={person.id}
+                                path={person.profile_path}
+                                name={person.name}
+                            />
+                        ))}
+                    </CreditTiles>
+                </CreditContainer>
+                <CreditContainer></CreditContainer>
+            </Wrapper>
+            <Footer
+                totalPages={totalPages}
+            />
+        </>
     );
 };
 
