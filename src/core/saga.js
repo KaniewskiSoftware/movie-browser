ddbc967a748469f408f8842cb57700bbeac7d9e9
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-03 21:20:31 +0100

    added people saga to saga

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
 import { movieDetailsSaga } from "../features/movieDetails/movieDetailsSaga";
 import { movieListSaga } from "../features/movieList/movieListSaga";
+import { peopleSaga } from "../features/peopleList/peopleSaga";
 
 export default function* saga() {
-  yield all([movieListSaga(), movieDetailsSaga()]);
+  yield all([movieListSaga(), movieDetailsSaga(), peopleSaga()]);
 }
