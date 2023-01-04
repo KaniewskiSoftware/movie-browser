01b6297f9e1137bf12c76f9f4d912c25e7aa3c87
Filip Buchwald <fixu1998@gmail.com>
2023-01-04 21:00:24 +0100

    fix warning for a non-boolean attribute

--- src/common/Footer/Button/index.js
+++ src/common/Footer/Button/index.js
@@ -3,7 +3,7 @@ import { StyledArrow, StyledButton, Title } from "./styled";
 const Button = ({ title, rotate, onClick, disabled, mobile }) => (
   <StyledButton disabled={disabled} onClick={onClick}>
     <StyledArrow $rotate={rotate}  />
-    <StyledArrow $rotate={rotate} mobile={mobile} hide />
+    <StyledArrow $rotate={rotate} mobile={mobile} hide="true" />
     <Title>{title}</Title>
   </StyledButton>
 );
