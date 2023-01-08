335b4cf61da495d18777373ed385f9d3a9b0462a
Filip Buchwald <fixu1998@gmail.com>
2023-01-08 14:21:49 +0100

    added conditions to make the component more generic

--- src/common/DetailsTile/index.js
+++ src/common/DetailsTile/index.js
@@ -1,6 +1,12 @@
 import { Fragment } from "react";
 import { Tag, Tags } from "../Tags";
-import { Rate, RateBox, SmallText, Star, TextBox } from "../../features/movieDetails/MovieDetailsPage/Movie/Rating/index";
+import {
+  Rate,
+  RateBox,
+  SmallText,
+  Star,
+  TextBox,
+} from "../../features/movieDetails/MovieDetailsPage/Movie/Rating/index";
 import {
   Container,
   Content,
@@ -34,43 +40,48 @@ const DetailsTile = ({
     </ImageBackground>
     <Content>
       <Title>{title}</Title>
-      <Year>{release.slice(0, 4)}</Year>
+      {release && <Year>{release.slice(0, 4)}</Year>}
       <Properties>
-        <Property>
-          <PropertyText entitled>Production:</PropertyText>
-          {production.map((country, index, countries) => (
-            <Fragment key={index}>
-              <PropertyText $big key={country.name}>
-                {country.name}
-                {countries.length > 0 ? (
-                  index < countries.length - 1 ? (
-                    <span>,&nbsp;</span>
+        {production && (
+          <Property>
+            <PropertyText entitled>Production:</PropertyText>
+            {production.map((country, index, countries) => (
+              <Fragment key={index}>
+                <PropertyText $big key={country.name}>
+                  {country.name}
+                  {countries.length > 0 ? (
+                    index < countries.length - 1 ? (
+                      <span>,&nbsp;</span>
+                    ) : (
+                      ""
+                    )
                   ) : (
                     ""
-                  )
-                ) : (
-                  ""
-                )}
-              </PropertyText>
-              <PropertyText $small key={country.iso_3166_1}>
-                {country.iso_3166_1}
-                {countries.length > 0 ? (
-                  index < countries.length - 1 ? (
-                    <span>,&nbsp;</span>
+                  )}
+                </PropertyText>
+                <PropertyText $small key={country.iso_3166_1}>
+                  {country.iso_3166_1}
+                  {countries.length > 0 ? (
+                    index < countries.length - 1 ? (
+                      <span>,&nbsp;</span>
+                    ) : (
+                      ""
+                    )
                   ) : (
                     ""
-                  )
-                ) : (
-                  ""
-                )}
-              </PropertyText>
-            </Fragment>
-          ))}
-        </Property>
-        <Property>
-          <PropertyText entitled>Release date:</PropertyText>
-          <PropertyText>{release}</PropertyText>
-        </Property>
+                  )}
+                </PropertyText>
+              </Fragment>
+            ))}
+          </Property>
+        )}
+
+        {release && (
+          <Property>
+            <PropertyText entitled>Release date:</PropertyText>
+            <PropertyText>{release}</PropertyText>
+          </Property>
+        )}
       </Properties>
       {genres && (
         <Tags details>
@@ -79,16 +90,18 @@ const DetailsTile = ({
           ))}
         </Tags>
       )}
-      <RateBox tile>
-        <Star $mini />
-        <TextBox>
-          <Rate tile>{vote.toFixed(1)}</Rate>
-          <SmallText tile $big>
-            / 10
-          </SmallText>
-          <SmallText tile>{votes} votes</SmallText>
-        </TextBox>
-      </RateBox>
+      {vote && votes && (
+        <RateBox tile>
+          <Star $mini />
+          <TextBox>
+            <Rate tile>{vote.toFixed(1)}</Rate>
+            <SmallText tile $big>
+              / 10
+            </SmallText>
+            <SmallText tile>{votes} votes</SmallText>
+          </TextBox>
+        </RateBox>
+      )}
       <Description big>{description}</Description>
     </Content>
     <Description small>{description}</Description>
