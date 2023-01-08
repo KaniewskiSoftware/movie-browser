347e80f6b93677aa8415748b88e20f181c146d40
Filip Buchwald <fixu1998@gmail.com>
2023-01-08 16:29:48 +0100

    removed unnecesary code

--- src/features/personDetails/personDetailsSaga.js
+++ src/features/personDetails/personDetailsSaga.js
@@ -1,41 +1,43 @@
 import { delay, takeLatest, call, put, select } from "redux-saga/effects";
-import { getPersonCredits, getPersonDetails } from "../../common/apiData/apiRequests";
-import { loadingDelay } from "../../common/states/loadingDelay";
-// import { getPersonCredits, getPersonDetails } from "./personDetailsAPI";
 import {
-    fetchCredits,
-    fetchPersonDetails,
-    fetchError,
-    fetchPersonDetailsSuccess,
-    selectPersonId,
-    fetchCreditsSuccess,
+  getPersonCredits,
+  getPersonDetails,
+} from "../../common/apiData/apiRequests";
+import { loadingDelay } from "../../common/states/loadingDelay";
+import {
+  fetchCredits,
+  fetchPersonDetails,
+  fetchError,
+  fetchPersonDetailsSuccess,
+  selectPersonId,
+  fetchCreditsSuccess,
 } from "./personDetailsSlice";
 
 function* fetchPersonDetailsHandler() {
-    try {
-        const id = yield select(selectPersonId);
+  try {
+    const id = yield select(selectPersonId);
 
-        yield put(fetchCredits());
-        yield delay(loadingDelay); //for loader demo purpose
-        const person = yield call(getPersonDetails, id);
-        yield put(fetchPersonDetailsSuccess(person));
-    } catch (error) {
-        yield put(fetchError());
-    }
+    yield put(fetchCredits());
+    yield delay(loadingDelay); //for loader demo purpose
+    const person = yield call(getPersonDetails, id);
+    yield put(fetchPersonDetailsSuccess(person));
+  } catch (error) {
+    yield put(fetchError());
+  }
 }
 
 function* fetchPersonCreditsHandler() {
-    try {
-        const id = yield select(selectPersonId);
+  try {
+    const id = yield select(selectPersonId);
 
-        const person = yield call(getPersonCredits, id);
-        yield put(fetchCreditsSuccess(person));
-    } catch (error) {
-        yield put(fetchError());
-    }
+    const person = yield call(getPersonCredits, id);
+    yield put(fetchCreditsSuccess(person));
+  } catch (error) {
+    yield put(fetchError());
+  }
 }
 
 export function* personDetailsSaga() {
-    yield takeLatest(fetchPersonDetails.type, fetchPersonDetailsHandler);
-    yield takeLatest(fetchCredits.type, fetchPersonCreditsHandler);
+  yield takeLatest(fetchPersonDetails.type, fetchPersonDetailsHandler);
+  yield takeLatest(fetchCredits.type, fetchPersonCreditsHandler);
 }
