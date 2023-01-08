e2b604740202c4b6a916e39ca1ae3af90bc0cae4
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-08 16:54:14 +0100

    fixed title, if no cast or crew

--- src/features/personDetails/PersonDetailsPage/Person/index.js
+++ src/features/personDetails/PersonDetailsPage/Person/index.js
@@ -7,32 +7,32 @@ import { selectCredits, selectPersonDetails } from "../../personDetailsSlice";
 import { DetailsWrapper } from "./styled";
 
 const Person = () => {
-  const personDetails = useSelector(selectPersonDetails);
-  const personCredits = useSelector(selectCredits);
+    const personDetails = useSelector(selectPersonDetails);
+    const personCredits = useSelector(selectCredits);
 
-  return (
-    <Wrapper>
-      <DetailsWrapper>
-        <DetailsTile
-          poster={personDetails.profile_path}
-          title={personDetails.name}
-          dateBirth={personDetails.birthday}
-          placeBirth={personDetails.place_of_birth}
-          description={personDetails.biography}
-        />
-        <Title title={`Movies - cast (${personCredits.cast.length})`} />
-        <MovieTiles
-          movies={personCredits.cast}
-          genres={personCredits.cast.genres_ids}
-        />
-        <Title title={`Movies - crew (${personCredits.crew.length})`} />
-        <MovieTiles
-          movies={personCredits.crew}
-          genres={personCredits.crew.genres_ids}
-        />
-      </DetailsWrapper>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <DetailsWrapper>
+                <DetailsTile
+                    poster={personDetails.profile_path}
+                    title={personDetails.name}
+                    dateBirth={personDetails.birthday}
+                    placeBirth={personDetails.place_of_birth}
+                    description={personDetails.biography}
+                />
+                <Title title={personCredits.cast.length > 0 ? `Movies - cast (${personCredits.cast.length})` : ""} />
+                <MovieTiles
+                    movies={personCredits.cast}
+                    genres={personCredits.cast.genres_ids}
+                />
+                <Title title={personCredits.crew.length > 0 ? `Movies - crew (${personCredits.crew.length})` : ""} />
+                <MovieTiles
+                    movies={personCredits.crew}
+                    genres={personCredits.crew.genres_ids}
+                />
+            </DetailsWrapper>
+        </Wrapper>
+    );
 };
 
 export default Person;
