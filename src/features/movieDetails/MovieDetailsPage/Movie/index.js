0fc84cb2bfc04d940de99d6b1949de54b49be179
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-09 11:54:05 +0100

    fixed rendering empy sections cast and crew

--- src/features/movieDetails/MovieDetailsPage/Movie/index.js
+++ src/features/movieDetails/MovieDetailsPage/Movie/index.js
@@ -37,34 +37,38 @@ const Movie = () => {
             votes={movieDetails.vote_count}
             description={movieDetails.overview}
           />
-          <CreditContainer>
-            <CreditHeader>Cast</CreditHeader>
-            <CreditTiles>
-              {credits.cast.map((actor) => (
-                <Credits
-                  key={actor.credit_id}
-                  path={actor.profile_path}
-                  name={actor.original_name}
-                  role={actor.character}
-                  id={actor.id}
-                />
-              ))}
-            </CreditTiles>
-          </CreditContainer>
-          <CreditContainer>
-            <CreditHeader>Crew</CreditHeader>
-            <CreditTiles>
-              {credits.crew.map((member) => (
-                <Credits
-                  key={member.credit_id}
-                  path={member.profile_path}
-                  name={member.original_name}
-                  role={member.job}
-                  id={member.id}
-                />
-              ))}
-            </CreditTiles>
-          </CreditContainer>
+          {credits.cast.length > 0 && (
+            <CreditContainer>
+              <CreditHeader>Cast</CreditHeader>
+              <CreditTiles>
+                {credits.cast.map((actor) => (
+                  <Credits
+                    key={actor.credit_id}
+                    path={actor.profile_path}
+                    name={actor.original_name}
+                    role={actor.character}
+                    id={actor.id}
+                  />
+                ))}
+              </CreditTiles>
+            </CreditContainer>
+          )}
+          {credits.crew.length > 0 && (
+            <CreditContainer>
+              <CreditHeader>Crew</CreditHeader>
+              <CreditTiles>
+                {credits.crew.map((member) => (
+                  <Credits
+                    key={member.credit_id}
+                    path={member.profile_path}
+                    name={member.original_name}
+                    role={member.job}
+                    id={member.id}
+                  />
+                ))}
+              </CreditTiles>
+            </CreditContainer>
+          )}
         </DetailsWrapper>
       </Wrapper>
     </>
