efd4fec94d480663af492a39374ba49c85358433
GrzegorzJendernal <g.jendernal@gmail.com>
2023-01-10 21:52:30 +0100

    changed get Api method to axios

--- src/common/apiData/apiRequests.js
+++ src/common/apiData/apiRequests.js
@@ -1,114 +1,79 @@
+import axios from "axios";
 import { apiKey } from "./apiKey";
 import { apiLink } from "./apiLink";
 
 export const getPersonDetails = async (id) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/person/${id}?api_key=${apiKey}&language=en`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getPersonCredits = async (id) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/person/${id}/movie_credits?api_key=${apiKey}&language=en`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getMovies = async (page) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/movie/popular?api_key=${apiKey}&page=${page}&language=en`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getGenres = async () => {
-  const response = await fetch(`${apiLink}/genre/movie/list?api_key=${apiKey}`);
+  const response = await axios.get(`${apiLink}/genre/movie/list?api_key=${apiKey}`);
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getMoviesByQuery = async (query, page) => {
   if (!query) {
     return;
   }
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=${page}`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getMovieDetails = async (id) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/movie/${id}?api_key=${apiKey}&language=en`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getCredits = async (id) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/movie/${id}/credits?api_key=${apiKey}&language=en`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getPeople = async (page) => {
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/person/popular?api_key=${apiKey}&page=${page}&language=en`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
 
 export const getPeopleByQuery = async (query, page) => {
   if (!query) {
     return;
   }
-  const response = await fetch(
+  const response = await axios.get(
     `${apiLink}/search/person?api_key=${apiKey}&language=en-US&query=${query}&page=${page}`
   );
 
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await response.data;
 };
