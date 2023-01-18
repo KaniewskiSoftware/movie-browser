cf2ffd163fafdab2f11af533754a2515eba0bfcb
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-01-18 22:15:30 +0100

    roles added for MovieTiles

--- src/common/MovieTiles/index.js
+++ src/common/MovieTiles/index.js
@@ -1,65 +1,72 @@
 import { Tag, Tags } from "../Tags";
 import {
-    Content,
-    GreyText,
-    Image,
-    ImageBackground,
-    MovieLink,
-    Rate,
-    Rating,
-    Star,
-    Tile,
-    Tiles,
-    Title
+  Content,
+  GreyText,
+  Image,
+  ImageBackground,
+  MovieLink,
+  Rate,
+  Rating,
+  Star,
+  Tile,
+  Tiles,
+  Title,
 } from "./styled";
 
 const MovieTiles = ({ genres, movies }) => (
-    <Tiles>
-        {movies.map((movie) => (
-            <MovieLink key={movies.indexOf(movie)} to={`/movies/${movie.id}`}>
-                <Tile>
-                    <ImageBackground>
-                        {movie.poster_path ? (
-                            <Image
-                                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                                alt=""
-                            />
-                        ) : (
-                            <Image />
-                        )}
-                    </ImageBackground>
-                    <Content>
-                        {movie.title && (
-                            <Title>{movie.title}</Title>
-                        )}
-                        {movie.release_date && (
-                            <GreyText>{movie.release_date.slice(0, 4)}</GreyText>
-                        )}
-                        {movie.genre_ids && genres ? (
-                            <Tags>
-                                {movie.genre_ids.map((genre_id) => (
-                                    <Tag key={genre_id}>
-                                        {genres.find((genre) => genre.id === genre_id).name}
-                                    </Tag>
-                                ))}
-                            </Tags>
-                        ) : (
-                            ""
-                        )}
-                        {movie.vote_average > 0 && (
-                            <Rating>
-                                <Star />
-                                <Rate>{movie.vote_average.toFixed(2)}</Rate>
-                                {movie.vote_count && (
-                                    <GreyText>{`${movie.vote_count} votes`}</GreyText>
-                                )}
-                            </Rating>
-                        )}
-                    </Content>
-                </Tile>
-            </MovieLink>
-        ))}
-    </Tiles>
+  <Tiles>
+    {movies.map((movie) => (
+      <MovieLink key={movies.indexOf(movie)} to={`/movies/${movie.id}`}>
+        <Tile>
+          <ImageBackground>
+            {movie.poster_path ? (
+              <Image
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt=""
+              />
+            ) : (
+              <Image />
+            )}
+          </ImageBackground>
+          <Content>
+            {movie.title && <Title>{movie.title}</Title>}
+            {movie.release_date && (movie.department || movie.character) ? (
+              <GreyText>
+                {movie.department || movie.character} (
+                {movie.release_date.slice(0, 4)})
+              </GreyText>
+            ) : movie.release_date ? (
+              <GreyText>{movie.release_date.slice(0, 4)}</GreyText>
+            ) : movie.department || movie.character ? (
+              <GreyText>{movie.department || movie.character}</GreyText>
+            ) : (
+              <></>
+            )}
+            {movie.genre_ids && genres ? (
+              <Tags>
+                {movie.genre_ids.map((genre_id) => (
+                  <Tag key={genre_id}>
+                    {genres.find((genre) => genre.id === genre_id).name}
+                  </Tag>
+                ))}
+              </Tags>
+            ) : (
+              ""
+            )}
+            {movie.vote_average > 0 && (
+              <Rating>
+                <Star />
+                <Rate>{movie.vote_average.toFixed(2)}</Rate>
+                {movie.vote_count && (
+                  <GreyText>{`${movie.vote_count} votes`}</GreyText>
+                )}
+              </Rating>
+            )}
+          </Content>
+        </Tile>
+      </MovieLink>
+    ))}
+  </Tiles>
 );
 
 export default MovieTiles;
