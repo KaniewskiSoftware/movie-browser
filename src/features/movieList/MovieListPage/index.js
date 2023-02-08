0878f63651373bf3bc856eb1cbf73f77310d58bb
GrzegorzJendernal <g.jendernal@gmail.com>
2023-02-08 23:15:00 +0100

    changed condition !!query to query !== null

--- src/features/movieList/MovieListPage/index.js
+++ src/features/movieList/MovieListPage/index.js
@@ -1,48 +1,40 @@
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchMovies,
-  setQuery,
-  selectStatus,
-  setPage,
-} from "../movieListSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchMovies, selectStatus, setPage, setQuery,} from "../movieListSlice";
 import ErrorPage from "../../../common/states/ErrorPage";
 import Loader from "../../../common/states/Loader";
-import { useEffect } from "react";
+import {useEffect} from "react";
 import Movies from "./Movies";
-import {
-  searchQueryParamName,
-  useQueryParameter,
-} from "../../../core/queryParameters";
-import { fetchGenres, selectIsGenres } from "../../../common/apiData/genres/genresSlice";
+import {searchQueryParamName, useQueryParameter,} from "../../../core/queryParameters";
+import {fetchGenres, selectIsGenres} from "../../../common/apiData/genres/genresSlice";
 
 const MovieListPage = () => {
-  const dispatch = useDispatch();
-
-  const status = useSelector(selectStatus);
-  const isGenres = useSelector(selectIsGenres);
-  const page = +useQueryParameter("page");
-  const query = useQueryParameter(searchQueryParamName);
-
-  useEffect(() => {
-    if (!isGenres) {
-      dispatch(fetchGenres());
-    }
-    if (!page) {
-      dispatch(setPage(1));
-    } else {
-      dispatch(setPage(page));
-    }
-    if (!!query) {
-      dispatch(setQuery(query));
-    }
-    dispatch(fetchMovies());
-  }, [dispatch,isGenres, page, query]);
-
-  return {
-    loading: <Loader />,
-    success: <Movies />,
-    error: <ErrorPage />,
-  }[status];
+	const dispatch = useDispatch();
+	
+	const status = useSelector(selectStatus);
+	const isGenres = useSelector(selectIsGenres);
+	const page = +useQueryParameter("page");
+	const query = useQueryParameter(searchQueryParamName);
+	
+	useEffect(() => {
+		if (!isGenres) {
+			dispatch(fetchGenres());
+		}
+		if (!page) {
+			dispatch(setPage(1));
+		} else {
+			dispatch(setPage(page));
+		}
+		if (query !== null) {
+			dispatch(setQuery(query));
+		}
+		dispatch(fetchMovies());
+	}, [dispatch, isGenres, page, query]);
+	
+	return {
+		loading: <Loader/>,
+		success: <Movies/>,
+		error: <ErrorPage/>,
+	}[status];
 };
 
 export default MovieListPage;
